47ce1936ce85f28e79f5458b8ee792095dcfc54a
Justin Swanson <justin.c.swanson@gmail.com>
2021-09-20 16:38:11 -0500

    Defaults can just be in the settings DTO now

--- ItemWeightCustomizer/Settings.cs
+++ ItemWeightCustomizer/Settings.cs
@@ -7,9 +7,27 @@ namespace ItemWeightCustomizer
     public partial class Settings
     {
         [JsonProperty("weightSettings")]
-        public WeightSettings WeightSettings { get; set; } = new WeightSettings();
+        public WeightSettings WeightSettings { get; set; } = new();
 
         [JsonProperty("categories")]
-        public HashSet<WeightCategory> Categories { get; set; } = new HashSet<WeightCategory>();
+        public HashSet<WeightCategory> Categories { get; set; } = new()
+        {
+            new WeightCategory("tools")
+            {
+                Types = new HashSet<string>() { "weapons" },
+                EditorIds = new HashSet<string>()
+                {
+                    "weapPickaxe",
+                    "Axe01",
+                    "DLC2AncientNordPickaxe",
+                    "DLC2RR03NordPickaxe",
+                    "DLC2dunKolbjornRalisPickaxe",
+                    "dunVolunruudPickaxe",
+                    "dunHaltedStreamPoachersAxe",
+                    "dunHaltedStreamPoachersAxeREPLICA"s
+                },
+                Weight = -1
+            }
+        };
     }
 }
